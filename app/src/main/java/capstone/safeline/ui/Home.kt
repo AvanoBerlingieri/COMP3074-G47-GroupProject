1a146b692ccf0becd8219bc1e6ec4ffb3c34c290
Avano <avanoberlingieri@gmail.com>
2025-10-22 17:32:16 -0400

    pages TODO

---
+++ app/src/main/java/capstone/safeline/ui/Home.kt
@@ -0,0 +1,10 @@
+package capstone.safeline.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class Home : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
